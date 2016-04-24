c4db506bb0d10c3ef16d2ed51b3ef3f9174384bf
Neptune <jhzhu@UranusZs-Mac-mini.local>
2016-04-24 08:08:57 +0800

    finish 102

--- tree/102_binary_tree_level_order_traversal.cc
+++ tree/102_binary_tree_level_order_traversal.cc
@@ -7,6 +7,10 @@
  *      Given binary tree {3,9,20,#,#,15,7},
  */
 
+#include <vector>
+#include <queue>
+#include <iostream>
+
 using namespace std;
 
 struct TreeNode {
@@ -14,16 +18,55 @@ struct TreeNode {
     TreeNode *left;
     TreeNode *right;
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- };
+};
 
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode *root) {
-        
+        vector<vector<int>> res;
+
+        if (!root)
+            return res;
+
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            unsigned long size = q.size();
+            vector<int> v;
+
+            for (unsigned long i = 0; i < size; ++i) {
+                auto node = q.front();
+                q.pop();
+                v.push_back(node->val);
+
+                if (node->left) q.push(node->left);
+
+                if (node->right) q.push(node->right);
+            }
+
+            res.push_back(v);
+        }
+
+        return res;
     }
 };
 
 int main() {
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->left->left = new TreeNode(3);
+    root->right = new TreeNode(4);
+    root->right->left = new TreeNode(6);
+    root->right->left->right = new TreeNode(6);
+    auto res = Solution().levelOrder(root);
+
+    for (auto &vec : res) {
+        for (auto n : vec)
+            cout << n << " ";
+
+        cout << endl;
+    }
 
     return 0;
 }
